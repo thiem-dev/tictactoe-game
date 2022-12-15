001e1adaf65b28bf37ea7f4d0b8b652dee014307
Thiem <thiem.huynh0@gmail.com>
2022-12-15 18:03:19 -0500

    almost finished. missing winner turn counter

--- game-logic.js
+++ game-logic.js
@@ -1,11 +1,9 @@
-console.log("first commit");
-
 // occasionally referring to: https://www.codebrainer.com/blog/tic-tac-toe-javascript-game
 
 const PLAYER_X = 'x';
-let player_x_boxes = [12,3,3];
+let player_x_boxes = [];
 const PLAYER_O = 'o';
-let player_o_boxes = [2,3,5];
+let player_o_boxes = [];
 const WINNING_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -18,15 +16,17 @@ const WINNING_COMBINATIONS = [
 ]
 
 let player_turn = 'x';
-
+let turnCount = 1;
 const cellElements = document.querySelectorAll(".cell");
+const BOARD_OBJ = document.querySelector("#board");
 const resetBtn = document.querySelector("#restartButton");
-
+const winningBox = document.querySelector("#winningMessage");
+const winningMessage = document.querySelector("#winningMessageText");
 console.log(cellElements);
 
 
 
-
+init();
 
 function init(){
 
@@ -38,40 +38,110 @@ function init(){
         })
     }
 
-
+    resetBtn.addEventListener("click", function(event){
+        restartGame();
+    })
 }
 
 function addSymbol(boxPickedObject){
+    console.log(turnCount + ":" + player_turn);
     switch (player_turn){
         case 'x':
-            console.log('player x turn');
             boxPickedObject.classList.add("mark-x");
+            boxPickedObject.classList.add("disabled-box");
             boxPickedObject.setAttribute("disabled", "disabled");
+            player_x_boxes.push(parseInt(boxPickedObject.getAttribute("value")));
             player_turn = 'o';
             break;
         case 'o':
-            console.log('player o turn');
             boxPickedObject.classList.add("mark-o");
+            boxPickedObject.classList.add("disabled-box");
             boxPickedObject.setAttribute("disabled", "disabled");
+            player_o_boxes.push(parseInt(boxPickedObject.getAttribute("value")));
             player_turn = 'x';
             break;
     }
 }
 
 function checkGrid(boxPickedObject){
-    if(player_x_boxes.length <= 2 || player_o_boxes.length <= 2){
-        console.log("not enough boxes to check");
-    }
-    else{
+    if(boxPickedObject.classList.contains(".disabled-box")){
+        console.log("box already has symbol");
+        return;
+    } else {
+        //checks for winning combos
         addSymbol(boxPickedObject);
-        console.log("enough marked");
+        turnCount++;
+
+        if(checkWinner() && turnCount >= 4){
+            console.log(player_turn);
+            winGame();
+        } 
     }
+
+    if(turnCount >= 10){
+        isDraw();
+        return;
+    }
+}
+
+function checkWinner(){
+    /* 
+    refer to for .some and .every method: https://bobbyhadz.com/blog/javascript-check-if-array-contains-all-elements-another-array
+
+    note to future self: .some and .every is like easier FOR loops of arrays
+        -some comparisons are true
+        -every comparison has to be true
+
+        winning_combinations: [a,b,c], [d,e,f]
+        combination: [a,b,c],
+        comboNumber: a
+        array1.includes(a)
+    */
+
+    return WINNING_COMBINATIONS.some(combination => {
+        // console.log(combination);
+        return combination.every(comboNumber => {
+            // console.log(comboNumber);
+            return player_o_boxes.includes(comboNumber);
+        })
+    })
+}
+
+
+
+function endGame(){
+    for(let i = 0; i < cellElements.length; i++){
+        cellElements[i].classList.add("disabled-box");
+    }
+    return;
+}
+
+function winGame(){
+    winningMessage.textContent = "The Winner is: "+ player_turn;
+    winningBox.classList.add("winner");
+    endGame();
+}
+
+function isDraw(){
+    // cellElements.every(cellElements.contains(".disabled-box"));
+    console.log("Game is a draw");
+    endGame();
+    return;
 }
 
 function restartGame(){
+    player_o_boxes = [];
+    player_x_boxes = [];
+    player_turn = 'x';
+    turnCount = 0;
 
+    winningMessage.textContent = "";
+    winningBox.classList.remove("winner");
+
+    for(let i = 0; i < cellElements.length; i++){
+        cellElements[i].classList.remove("mark-x");
+        cellElements[i].classList.remove("mark-o");
+        cellElements[i].classList.remove("disabled-box");
+    }
+    return; 
 }
-
-
-
-init();
